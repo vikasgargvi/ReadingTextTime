c8633229f3b38eba4bda2b512c23629d793bdf41
vikas garg <33121984+vikasgargvi@users.noreply.github.com>
2018-10-19 23:46:26 +0530

    Update ReadingTime.py

--- ReadingTime.py
+++ ReadingTime.py
@@ -1,11 +1,15 @@
 import math
 
 class CalculateTime():
-
-	def calTime(text):
-		length = len(text.split())
-		time = length/200
-		minutes = math.floor(time)
-		seconds = (time-minutes)*60
-		seconds = math.ceil(seconds) if math.ceil(seconds)-seconds < 0.5 else math.floor(seconds)
-		return str(minutes) + " minutes " + str(seconds) + " seconds "
+        def calTime(text):
+                length = len(text.split())
+                time = length/200
+                minutes = math.floor(time)
+                seconds = (time-minutes)*60
+                seconds = math.ceil(seconds) if math.ceil(seconds)-seconds < 0.5 else math.floor(seconds)
+                if minutes < 1:
+                        return str(length) + " less than 1 minute"
+                elif seconds <= 30:
+                        return str(length) + " about " + str(minutes) + " min"
+                else:
+                        return str(length) + " about " + str(minutes + 1) + " min "
